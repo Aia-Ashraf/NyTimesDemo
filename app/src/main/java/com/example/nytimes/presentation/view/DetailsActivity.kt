2fb1e84b40714d4814972f12d0eea10f296eec61
Aia Ashraf <aia.ashraf94@gmail.com>
2020-05-08 13:54:08 -0700

    init

--- app/src/main/java/com/example/nytimes/presentation/view/DetailsActivity.kt
+++ app/src/main/java/com/example/nytimes/presentation/view/DetailsActivity.kt
@@ -10,11 +10,19 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+        getData()
+    }
+
+    private fun getData() {
         val intent = intent
         val title: String = intent.getStringExtra("title")
         val description: String = intent.getStringExtra("des")
 
-        tv_details_description.text =description
-        tv_details_title.text =title
+        bindData(description, title)
+    }
+
+    private fun bindData(description: String, title: String) {
+        tv_details_description.text = description
+        tv_details_title.text = title
     }
 }
